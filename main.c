ee7a6937abbad1dd6a3239390855432a06c1f59c
HP <HP@DESKTOP-OHUQA4D>
2021-03-16 22:52:45 -0600

    REPOSITORIO DE ALGORÍTMOS Y ESTRUCTURAS DE DATOS EN LENGUAJE C++

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: HP
+ *
+ * Created on 16 de marzo de 2021, 10:51 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=algoritmos_y_estructura_de_datos
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/algoritmos_y_estructura_de_datos
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=algoritmosyestructuradedatos.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/algoritmosyestructuradedatos.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=algoritmos_y_estructura_de_datos
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/algoritmos_y_estructura_de_datos
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=algoritmosyestructuradedatos.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/algoritmosyestructuradedatos.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
